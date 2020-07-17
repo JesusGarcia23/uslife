9ade945bd62a059819b48dc5e20712921d180f0a
= <jacex17@gmail.com>
2020-07-16 21:51:17 -0700

    change layout for login and signup page

--- src/views/LandingPage/styles.js
+++ src/views/LandingPage/styles.js
@@ -5,15 +5,14 @@ export const Container = styled.div`
 width: 99.8vw;
 height: 99.8vh;
 display: flex;
-justify-content: center;
+justify-content: flex-end;
 align-items: center;
-background: url(./assets/images/Signup.jpg) no-repeat center;
+background: url(./assets/images/LandingPage.jpg) no-repeat center;
 `
 
 export const Content = styled.div`
-height: 60vh;
-width: 25vw;
-border-radius: 2%;
-background: rgba(255,255,255, 0.95);
+height: 100vh;
+width: 35vw;
+background: rgba(46, 49, 49, 0.95);
 box-shadow: 2px 3px 3px 0 rgba(0, 0, 0, 0.6), 0 2px 20px 0 rgba(0, 0, 0, 0.69);
 ` 
