f498a8f699883a5fa9df14e018a5ac1089e41edd
= <jacex17@gmail.com>
2020-07-18 11:36:36 -0700

    add more inputs and styling to login page

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {Container, Content, Form } from './styles';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
+
+    const [email, setEmail] = useState('');
+    
+    const [password, setPassword] = useState('');
+
+    const onSubmitLogin = (event) => {
+        event.preventDefault();
+        console.log("Hello")
+    }
+
+    const goToSignup = () => {
+        props.history.push('/signup')
+    }
+
     return (
         <Container>
             <Content>
                 <h1>Login</h1>
-                <Form>
-                    <input placeholder='email'></input>
-                    <input placeholder='password'></input>
+                <Form onSubmit={e => onSubmitLogin(e)}>
+                    <input placeholder='email' value={email} onChange={e => setEmail(e.target.value)}></input>
+                    <input placeholder='password' value={password} onChange={e => setPassword(e.target.value)}></input>
+                    <button type='submit'>Log In</button>
                 </Form>
+                <Link to='/forgotpassword'>Forgot password?</Link>
+
+                <button onClick={goToSignup}>Don't have an account yet? Register here</button>
             </Content>
         </Container>
     )
--- src/views/LandingPage/styles.js
+++ src/views/LandingPage/styles.js
@@ -9,7 +9,7 @@ justify-content: flex-end;
 align-items: center;
 background: url(./assets/images/LandingPage.jpg) no-repeat center;
 
-@media (max-width: 768px) {
+@media (max-width: 900px) {
     justify-content: center;
   }
 `
@@ -21,16 +21,40 @@ justify-content: center;
 align-items: center;
 height: 100vh;
 width: 35vw;
-background: rgba(46, 49, 49, 0.95);
+background: rgba(46, 49, 49, 0.97);
 box-shadow: 2px 3px 3px 0 rgba(0, 0, 0, 0.6), 0 2px 20px 0 rgba(0, 0, 0, 0.69);
 > h1 {
-    font-size: 7vh;
+    font-size: 60px;
     color: white;
     margin-bottom: 50px;
 }
 
-@media (max-width: 768px) {
-    width: 75vw;
+> a {
+  margin: 20px;
+  text-decoration: none;
+  color: #649cf7;
+}
+
+> button {
+  margin-top: 30px;
+  height: 40px;
+  width: 340px;
+  border: 0px;
+  background-color: #806130;
+  border-radius: 2px;
+  font-size: 15px;
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+};
+
+> button:hover {
+  cursor: pointer;
+  background-color: #be924b;
+}
+
+@media (max-width: 900px) {
+    width: 85vw;
   }
 ` 
 
@@ -39,7 +63,6 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-border: 1px solid red;
 width: 50%;
  > input {
      border: 0px;
@@ -47,5 +70,24 @@ width: 50%;
      height: 35px;
      width: 80%;
      margin: 20px 0;
+     text-align: center;
+ };
+
+ > button {
+   font-size: 20px;
+   color: white;
+   font-weight: bold;
+   background-color: #3e6199;
+   margin-top: 10px;
+   height: 45px;
+   width: 100px;
+   border: 0px;
+   border-radius: 5px;
+ }
+
+ > button:hover {
+   cursor: pointer;
+   background-color: #4b77be;
+
  }
 `
